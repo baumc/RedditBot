97e452ccd25097388fba14bd2b94918f13e508a6
baumc <baum.c.j@gmail.com>
2016-09-07 00:37:44 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -9,5 +9,8 @@ links = t.xpath('//li[@class="entry"]/a/@href')
 names = t.xpath('//li[@class="entry"]/a/h4/text()')
 for l,n in zip(links, names):
 	if "2016/09" in n:
-		temp += "[**"+n.strip()+"**]("+l+")"
+		temp += ">>[**"+n.strip()+"**]("+l+")\n"
 		print temp
+
+		##TODO change to get from current month
+		##
